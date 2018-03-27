4142201d4418791b1d9dc09f2db3a3f05dc79f85
Theodor <32040995+theoduf@users.noreply.github.com>
2018-03-27 15:14:27 +0200

    `r added for newline printing in txt

--- procmi.ps1
+++ procmi.ps1
@@ -20,6 +20,6 @@ foreach($i in $args){
   $process=$(Get-Process | Where-Object{$_.Id -eq $i})
   $vm=$($process.vm | .\human-readable-bytes.ps1)
   $ws=$($process.ws | .\human-readable-bytes.ps1)
-  New-Item ".\$i--$now.meminfo" -ItemType file -Force -Value "******** Minne info om prosess med PID $i ******** `n Total bruk av virtuelt minne: $vm `n St`ørrelse p`å Working Set: $ws"
+  New-Item ".\$i--$now.meminfo" -ItemType file -Force -Value "******** Minne info om prosess med PID $i ******** `r`n Total bruk av virtuelt minne: $vm `r`n St`Ã¸rrelse p`Ã¥ Working Set: $ws"
 
 }
